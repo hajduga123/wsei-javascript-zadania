aa47346c2ade1c1a201c9c0b91078dc0a5bc8be7
hajduga123 <48643496+hajduga123@users.noreply.github.com>
2019-11-24 14:17:21 +0100

    Add files via upload

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,27 @@
+console.log9'bbb'0
 document.addEventListener("DOMContentLoaded", function () {
+    console.log('aaa')
+    let elemClass = document.getElementsByClassName('title')[0];
+
+    function getDataAnimation(element){
+        console.log('ccc');
+        let newVariable = element.getAttribute('data-animation');
+        console.log('ddd',newVarible);
+        return newVariable;
+
+    }
+    getDataAnimation(elemClass);
     /*
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
+    var el1 = document.getElementById('home');
+    var el2 = document.querySelector('#home');
+
+    var eli = document.querySelector('li[data-direction]')[0];
+    var elClassBlock = document.querySelector('.block');
+
+    var allLi = document.querySelectorAll('nav li');
+    var allP = document.querySelector('div p');
+    var allDiv = document.querySelectorAll('article div')
 
 });
